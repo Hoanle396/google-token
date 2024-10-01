ae35c3a7f74c3c7f93746e2c6920bbf923f7e3b0
Lê Hữu Hoàn <89791546+Hoanle396@users.noreply.github.com>
2024-10-01 23:29:58 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { gapi } from "gapi-script";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GoogleLogin } from "react-google-login";
 
 function App() {
+  const [data,setData] = useState("");
   useEffect(() => {
     gapi.load("client:auth2", () => {
       gapi.client.init({
@@ -13,17 +14,24 @@ function App() {
     });
   }, []);
   const responseGoogle = (response) => {
-    console.log(response);
+    setData(response);
   };
 
   return (
-    <GoogleLogin
-      clientId="970416655854-lshvdvota7og26upes5tij7n9vslke8g.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={"single_host_origin"}
-    />
+    <>
+      <GoogleLogin
+        clientId="970416655854-lshvdvota7og26upes5tij7n9vslke8g.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+      <pre>   
+        <code>
+          {data}
+        </code>
+      </pre>
+    </>
   );
 }
 
