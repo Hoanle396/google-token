67ff1508289c74efb810fb69db9c84aa22245890
Le Huu Hoan <hoanle396@gmail.com>
2025-04-24 15:54:12 +0700

    feat: add random array generation functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { useLogin, usePrivy } from "@privy-io/react-auth";
 import { gapi } from "gapi-script";
 import { useEffect, useState } from "react";
 import { GoogleLogin } from "react-google-login";
 import ReactJson from "react-json-view";
-import { useLogin, usePrivy } from "@privy-io/react-auth";
 
 function App() {
   const [data, setData] = useState({});
+  const [limit, setLimit] = useState(1);
   const { getAccessToken, authenticated } = usePrivy();
   const { login } = useLogin();
 
@@ -32,6 +33,27 @@ function App() {
     setData(response);
   };
 
+  const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const generateUniqueRandomArray = (size, min, max) => {
+    const set = new Set();
+    while (set.size < size) {
+      set.add(getRandomInt(min, max));
+    }
+    return Array.from(set).sort((a, b) => Number(a) - Number(b));
+  };
+
+  const gen = () => {
+    const result = [];
+    for (let i = 0; i < limit; i++) {
+      const randomArray = generateUniqueRandomArray(6, 1, 55);
+      result.push(randomArray);
+    }
+    setData(result);
+  }
+
   return (
     <>
       <GoogleLogin
@@ -52,6 +74,12 @@ function App() {
       >
         Login Privy
       </button>
+      <input
+        type="number"
+        value={limit}
+        onChange={(e) => setLimit(Number(e.target.value))}
+        />
+      <button onClick={gen}>Generate</button>
       <ReactJson src={data} />
     </>
   );
